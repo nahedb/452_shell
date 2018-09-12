0e70d6c4de19ae78c37cba4c1e3dcb0be6680ecc
hubersh <hunterhubers@gmail.com>
2018-09-12 17:54:00 -0400

    stashing changes

--- shell.c
+++ shell.c
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 
 int display_prompt(){
-    char hostname[1024];
-    gethostname(hostname, 1023);
-    printf("[%s]: ",hostname);
+    // char hostname[1024];
+    // gethostname(hostname, 1023);
+    // printf("[%s]: ",hostname);
+    printf("[shell]:");
     return 0;
 }
 
@@ -32,20 +33,39 @@ int main(int argc, char* argv[])
     printf("Welcome to my Shell! \nPlease enter a command.\n");
     pid_t pid;
     char command[300], quit[5] = "quit";
-    char* token = NULL;
+    // char* token = NULL;
     int status;
     while(1){
         display_prompt();
         fgets(command, 300, stdin);
-        while(command[0] =='\n' || command[0] == ' '){
+        while(command[0] == '\n' || command[0] == ' '){
             display_prompt();
             fgets(command, 256, stdin);
         }   
-        token = strtok(command, "\n");
-        if(strcmp(&token[0], quit) == 0){
-            printf("Quit\n");
-            break;
+
+
+        char* args[];
+        char* token = strtok(command, " ");
+
+        int counter = 0;
+        while (token != NULL) { 
+            printf("%s ", token); 
+            argv[counter] = token;
+            counter++;
+            token = strtok(NULL, " "); 
+        } 
+        
+        argv[counter] = NULL;
+        
+        for (int i = 0; i < argv; ++i)
+        {
+            printf("argv[%d]: %s\n", i, argv[i]);
         }
+
+                
+        printf("Hit\n");
+
+      
         pid = fork();
         if(pid < 0){
             perror("fork failure"); 
@@ -55,13 +75,8 @@ int main(int argc, char* argv[])
             waitpid(pid, &status, 0);
         }
         else{
-
-            argv = .split
-            argv[0] = "ls"; 
-            argv[1] = "-l";
-            argv[2] = NULL;
             // printf("%s\n", token);
-            execvp(token, argv);
+            execvp(argv[0], argv);
             printf("Command not known.\n");
             exit(1);
         }
