9888145605f2b8fd9da72459d71a63580357b382
hubersh <hunterhubers@gmail.com>
2018-09-12 21:57:16 -0400

    Cleaned up comments

--- shell.c
+++ shell.c
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 #include <sys/resource.h>
 
-//Brendan Nahed and Hunter Hubers
-//CIS 452 Computer Operating Systems
-//September 10, 2018
+/* Brendan Nahed and Hunter Hubers
+*  CIS 452 Computer Operating Systems
+*  September 10, 2018
+*/
 
 int display_prompt(){
-    //This is the display prompt to the user
-    //that is before every command. 
+    // This is the display prompt to the user that is before every command. 
     char hostname[1024];
     gethostname(hostname, 1023);
     printf("[%s]: ",hostname);
@@ -19,26 +19,29 @@ int display_prompt(){
 }
 
 int read_commands(char* cmd, char** token){
+    
     char *pos;
-    //Operation to remove the \n at the end of
-    //the char array.
+
+    // Operation to remove the \n at the end of the char array.
     if((pos=strchr(cmd, '\n')) != NULL){
         *pos = '\0';
     }   
     int count = 0;
     char * tempcmd;
     tempcmd = strtok(cmd, " ");
-    //This operation tokenizes the command and 
-    //stores each token initially into tempcmd
-    //and then into the array of token at count.
+
+    /* This operation tokenizes the command and stores each token 
+    *  initially into tempcmd and then into the array of token at count.
+    */
     while(tempcmd != NULL){
         token[count] = tempcmd;
         tempcmd = strtok(NULL, " ");
         count++;
     }
-    //Adds a NULL statement in the last place in
-    //the Char array of token. This allows execvp
-    //to stop at NULL.
+
+    /* Adds a NULL statement in the last place in the Char array 
+    *  of token. This allows execvp to stop at NULL.
+    */
     token[count] = NULL;
     return 0;
 }
@@ -46,10 +49,10 @@ int read_commands(char* cmd, char** token){
 int main(int argc, char* argv[]) {
     printf("Welcome to my Shell! \nPlease enter a command.\n");
     
-    //pid is the Child id. 
+    // pid is the Child id. 
     pid_t pid;
     
-    //Command in the input recieved from the user.
+    // Command in the input recieved from the user.
     char command[300];
     
     struct rusage x; 
@@ -57,18 +60,17 @@ int main(int argc, char* argv[]) {
     struct timeval end;
     
 
-    //Token is a char array to store the tokenized
-    //command from the user.
+    // Token is a char array to store the tokenized command from the user.
     char* token[15];
     
-    //Status holds the status of the child.
+    // Status holds the status of the child.
     int status;
     
     while(1){
         display_prompt();
         fgets(command, 300, stdin);
         
-        //While loop to check for empty commands.
+        // While loop to check for empty commands.
         while(command[0] =='\n' || command[0] == ' '){
             display_prompt();
             fgets(command, 300, stdin);
@@ -78,31 +80,27 @@ int main(int argc, char* argv[]) {
             break;
         }
         
-        //Fork to create the parent and child. Stores
-        //the Child in pid.
+        // Fork to create the parent and child. Stores the Child in pid.
         pid = fork();
         
         getrusage(RUSAGE_SELF, &x);
         start=x.ru_utime;
 
-        //Failed fork catch.
+        // Failed fork catch.
         if(pid < 0){
             perror("fork failure"); 
             exit(1); 
         }
 
-        //Shell Blocks.
+        // Shell Blocks.
         else if (pid){
             waitpid(pid, &status, 0);
-
             getrusage(RUSAGE_SELF, &x);
             end = x.ru_utime;
+            // Print Resource Usage Statistics
             printf("User CPU time was: %ld.%ds\n", end.tv_sec-start.tv_sec, end.tv_usec-start.tv_usec);
             printf("There where %ld involuntary context switches.\n", x.ru_nivcsw);
-        }//Child Runs.
-        else{
-
-            // printf("%s\n", x.ru_utime);
+        } else { // Child Runs
             execvp(token[0], token);
             printf("Command not known.\n");
             exit(1);
